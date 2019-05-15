9ca219b9b38ac87802ce8df58b323f13bc050517
informatykastosowana <jakub.tomal@smcebi.edu.pl>
2019-05-15 19:06:57 +0200

    adding waves managment

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] List<WaveConfig> wavesconfig;
+    [SerializeField] bool looping;
+    int startingWaveIndex = 0;
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        do
+        {
+            yield return StartCoroutine(SpawnAllWaves());
+        } while (looping);
+        
+    }
+
+    private IEnumerator SpawnAllWaves()
+    {
+        for ( int waveIndex = startingWaveIndex; waveIndex < wavesconfig.Count ; waveIndex++ )
+        {
+            var currentWave = wavesconfig[waveIndex];
+            yield return StartCoroutine(SpawnAllEnemiesInWave(currentWave));
+        }
+
+    }
+
+    private IEnumerator SpawnAllEnemiesInWave(WaveConfig waveConfig)
+    {
+        for (int enemyCount = 0; enemyCount < waveConfig.GetNumberOfEnemies(); enemyCount++)
+        {
+            var newEnemy = Instantiate(waveConfig.GetEnemyPrefab(), waveConfig.GetWaypoints()[0].transform.position, Quaternion.identity);
+
+            newEnemy.GetComponent<EnemyPathing>().SetWaveConfig(waveConfig);
+
+            yield return new WaitForSeconds(waveConfig.GetTimeBeteewneSpawns());
+        }
+    }
+}
